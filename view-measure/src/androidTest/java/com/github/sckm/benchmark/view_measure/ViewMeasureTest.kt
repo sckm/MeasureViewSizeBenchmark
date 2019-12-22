7ae67d26aeb2192d78ca02269b867a2328ca1eae
scache <scal.che@gmail.com>
2019-12-23 00:07:36 +0900

    add benchmark for measuring view size

--- view-measure/src/androidTest/java/com/github/sckm/benchmark/view_measure/ViewMeasureTest.kt
+++ view-measure/src/androidTest/java/com/github/sckm/benchmark/view_measure/ViewMeasureTest.kt
@@ -1,7 +1,12 @@
 package com.github.sckm.benchmark.view_measure
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View.MeasureSpec
+import android.view.ViewGroup
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
@@ -13,9 +18,35 @@ class ViewMeasureTest {
     val benchmarkRule = BenchmarkRule()
 
     @Test
-    fun benchmarkSomeWork() {
+    fun noNestedWeights() {
         benchmarkRule.measureRepeated {
-            Thread.sleep(100)
+            val container: ViewGroup = runWithTimingDisabled {
+                val context = ApplicationProvider.getApplicationContext<Context>()
+                LayoutInflater.from(context).inflate(R.layout.layout_no_nested, null) as ViewGroup
+            }
+            measureAndLayoutWrapLength(container)
         }
+    }
+
+
+    @Test
+    fun nestedWeights() {
+        benchmarkRule.measureRepeated {
+            val container: ViewGroup = runWithTimingDisabled {
+                val context = ApplicationProvider.getApplicationContext<Context>()
+                LayoutInflater.from(context).inflate(R.layout.layout_nested, null) as ViewGroup
+            }
+            measureAndLayoutWrapLength(container)
+        }
+    }
+
+    private fun measureAndLayoutWrapLength(container: ViewGroup) {
+        val widthMeasureSpec = MeasureSpec.makeMeasureSpec(360, MeasureSpec.EXACTLY)
+        val heightMeasureSpec = MeasureSpec.makeMeasureSpec(640, MeasureSpec.AT_MOST)
+        container.measure(widthMeasureSpec, heightMeasureSpec)
+        container.layout(
+            0, 0, container.measuredWidth,
+            container.measuredHeight
+        )
     }
 }
